c48d6cc22057393433813d168d69eb2f03106f68
Lucas <lucas.humbert.28@gmail.com>
2024-01-18 16:00:02 +0100

    wip

--- index.js
+++ index.js
@@ -7,16 +7,176 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-
 app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'public/index.html'));
 });
 
+let users = {}
+let sondages = {}
+
 io.on('connection', (socket) => {
-    console.log('nouveau user')
-});
+    let currentSondageId = -1;
+    sendConnectedUsersToAll()
+
+    socket.on('user connexion', (pseudo) => {
+        if (!users[socket.id]) {
+            users[socket.id] = { pseudo: pseudo }
+            sendConnectedUsersToAll()
+            sendSondageList()
+        }
+    })
+
+    socket.on('disconnect', () => {
+        if (users[socket.id]) {
+            delete users[socket.id]
+            sendConnectedUsersToAll()
+            sendSondageListToAll()
+        }
+    })
+
+    socket.on('join sondage', (id) => {
+        currentSondageId = id;
+        socket.join(currentSondageId);
+
+        let isOrganisateur = sondages[id].organisateur.id === socket.id
+
+        if (users[socket.id]) { // TODO   && !isOrganisateur
+            sondages[id].joueurs[socket.id] = { pseudo: users[socket.id].pseudo }
+        }
+
+        sendSondagePlayersToAll(id)
+
+        let activeQuestion = getSondageActiveQuestion(id)
+
+        if (activeQuestion) {
+            isOrganisateur ? sendQuestion('non') : sendQuestion(activeQuestion)// TODO remplacer par envoyer les résultats
+        }
+    })
+
+    socket.on('leave sondage', () => {
+        socket.leave(currentSondageId)
+        if (sondages[currentSondageId]) {
+            delete sondages[currentSondageId].joueurs[socket.id]
+            sendSondagePlayersToAll(currentSondageId)
+            currentSondageId = -1
+        }
+        sendSondageList()
+        sendSondagePlayersToAll()
+    })
+
+    socket.on('create sondage', (sondageName) => {
+        let newId = Object.keys(sondages).length + 1
+        sondages[newId] = {
+            nom: sondageName,
+            joueurs: {},
+            organisateur: {
+                id: socket.id,
+                pseudo: users[socket.id].pseudo
+            },
+            questions: {}
+        }
+
+        sendSondageListToAll()
+    })
+
+    socket.on('org create question', (idSondage, question, reponse1, reponse2) => {
+        let sondage = sondages[idSondage]
+        if (sondage) {
+            let newId = Object.entries(sondage.questions).length + 1
+            let newQuestion = {
+                question: question,
+                reponse1: {
+                    libelle: reponse1,
+                    nbVotes: 0
+                },
+                reponse2: {
+                    libelle: reponse2,
+                    nbVotes: 0
+                },
+                active: true
+            }
+            sondage.questions[newId] = newQuestion
+
+            sendQuestionToAllPlayers(newQuestion)
+        }
+    })
+
+    socket.on('player response', (idSondage, reponse) => {
+        let question = getSondageActiveQuestion(idSondage)
+
+        if (question) {
+            if (reponse === 1) {
+                sondages[idSondage].questions[question.id].reponse1.nbVotes += 1
+            } else if (reponse === 2) {
+                sondages[idSondage].questions[question.id].reponse2.nbVotes += 1
+            }
+
+            sendResultats(idSondage)
+        }
+    })
+
+    function sendConnectedUsersToAll() {
+        io.emit('get connected users', users)
+    }
+
+    function sendSondageList() {
+        socket.emit('get sondages list', getActiveSondages())
+    }
+
+    function sendSondageListToAll() {
+        io.emit('get sondages list', getActiveSondages())
+    }
+
+    function getActiveSondages() {
+        let result = {}
+        for (const [id, sondage] of Object.entries(sondages)) {
+            if (users[sondage.organisateur.id]) {
+                result[id] = sondage
+            }
+        }
+
+        return result
+    }
+
+    function sendSondagePlayersToAll(sondageId) {
+        if (sondages[sondageId]) {
+            io.to(currentSondageId).emit('sondage players', sondages[sondageId].joueurs)
+        }
+    }
+
+    function sendQuestionToAllPlayers(question) {
+        socket.broadcast.to(currentSondageId).emit("question to players", question, currentSondageId);
+    }
+
+    function sendQuestion(question) {
+        socket.emit("question to players", question, currentSondageId);
+    }
+
+    function getSondageActiveQuestion(idSondage) {
+        let result = {}
+        for (const [id, question] of Object.entries(sondages[idSondage].questions)) {
+            if (question.active) {
+                question['id'] = id
+                result = question
+                break;
+            }
+        }
+
+        return Object.entries(result).length ? result : null
+    }
+
+    function sendResultats(idSondage) {
+        let question = getSondageActiveQuestion(idSondage)
+
+        if (question) {
+            console.log(sondages[idSondage].questions[question.id].reponse1.nbVotes)
+        }
+    }
+})
+
+
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
--- public/client.js
+++ public/client.js
@@ -1,1 +1,140 @@
 const socket = io();
+
+let nbUsers = document.getElementById('nb_users')
+let pseudoForm = document.getElementById('pseudo_form')
+let input = document.getElementById('pseudo_input')
+let button = document.getElementById('pseudo_btn')
+let sondageList = document.getElementById('list')
+let createForm = document.getElementById('create_form')
+let sondageContainer = document.getElementById('sondage_container')
+let backBtn = document.getElementById('back_btn')
+let sondageContent = document.getElementById('sondage_content')
+let sondageResultats = document.getElementById('sondage_resultats')
+
+
+button.addEventListener('click', () => {
+    socket.emit('user connexion', input.value)
+
+    pseudoForm.style.display = 'none'
+    sondageList.style.display = 'block'
+    createForm.style.display = 'block'
+
+    document.getElementById('infos').style.display = "block"
+    document.getElementById('your_pseudo').innerText = input.value
+})
+
+document.getElementById('new_sondage_btn').addEventListener('click', () => {
+    createSondage()
+})
+
+socket.on('get connected users', (users) => {
+    nbUsers.innerText = Object.entries(users).length.toString()
+})
+
+socket.on('get sondages list', (sondages) => {
+    sondageList.innerText = ''
+
+    if (Object.entries(sondages).length) {
+        for (const [id, sondage] of Object.entries(sondages)) {
+            const item = document.createElement('button')
+            item.textContent = sondage.nom
+            sondageList.appendChild(item)
+
+            item.addEventListener('click', () => {
+                joinSondage(id, sondage)
+            })
+        }
+    } else {
+        sondageList.innerText = 'Aucun sondage en cours'
+    }
+})
+
+socket.on('sondage players', (players) => {
+    let playerList= document.getElementById('sondage_players')
+    playerList.innerText = ''
+    for (const [id, player] of Object.entries(players)) {
+        const item = document.createElement('li');
+        item.textContent = player.pseudo;
+        playerList.appendChild(item);
+    }
+})
+
+socket.on('question to players', (question, sondageId) => {
+    sondageContent.innerHTML = `
+        Nouvelle question:
+        <br />
+        ${question.question}
+        <br />
+
+        <button id="reponse_1_btn">${question.reponse1.libelle}</button>
+        <button id="reponse_2_btn">${question.reponse2.libelle}</button>
+    `
+
+    document.getElementById('reponse_1_btn').addEventListener('click', () => {
+        envoyerReponse(sondageId, 1)
+    })
+
+    document.getElementById('reponse_2_btn').addEventListener('click', () => {
+        envoyerReponse(sondageId, 2)
+    })
+})
+
+function joinSondage(id, sondage) {
+    let sondageName = document.getElementById('sondage_name')
+    backBtn.addEventListener('click', () => {
+        leaveSondage()
+    })
+
+    sondageList.style.display = 'none'
+    createForm.style.display = 'none'
+    sondageContainer.style.display = 'block'
+
+    socket.emit('join sondage', id)
+
+    sondageName.innerText = 'Sondage ' + sondage.nom + ' de ' + sondage.organisateur.pseudo
+
+    if (sondage.organisateur.id === socket.id) {
+        afficherSondageForm(id)
+    } else {
+        sondageContent.innerText = 'En attente de l\'organisateur...'
+    }
+}
+
+function leaveSondage() {
+    socket.emit('leave sondage')
+
+    sondageContainer.style.display = 'none'
+    sondageList.style.display = 'block'
+    createForm.style.display = 'block'
+}
+
+function createSondage() {
+    let sondageName = document.getElementById('new_sondage_input')
+
+    if (sondageName.length !== 0) {
+        socket.emit('create sondage', sondageName.value)
+
+        sondageName.value = ''
+    }
+}
+
+function afficherSondageForm(idSondage) {
+    document.getElementById('organisateur_form').style.display = 'block'
+
+    document.getElementById('organisateur_btn').addEventListener('click', () => {
+        let question = document.getElementById('organisateur_question').value
+        let reponse1 = document.getElementById('reponse_1_input').value
+        let reponse2 = document.getElementById('reponse_2_input').value
+
+        socket.emit('org create question', idSondage, question, reponse1, reponse2)
+
+        document.getElementById('organisateur_form').style.display = 'none'
+        sondageResultats.style.display = 'block'
+        sondageResultats.innerText = 'En attente des votes...'
+    })
+}
+
+function envoyerReponse(idSondage, reponse) {
+    socket.emit('player response', idSondage, reponse)
+    sondageContent.innerText= ''
+}
